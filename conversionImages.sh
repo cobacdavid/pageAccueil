40a105a791620a1ba68611cfe3b77e0ae0ebf8d8
cobacdavid <7426305+cobacdavid@users.noreply.github.com>
2020-04-02 13:33:10 +0200

    .png + blur effect

--- conversionImages.sh
+++ conversionImages.sh
@@ -1,3 +1,9 @@
+rm back-*
 for image in *.png; do
-    convert $image -virtual-pixel transparent -modulate 100,50,100 -distort Perspective '0,0,200,50 0,1000,200,900 1920,0,1400,300 1920,1200,1400,900'  -scale 25% -scale 400% back-$image
+    convert $image \
+            -virtual-pixel transparent \
+            -modulate 100,50,100 \
+            -distort Perspective '0,0,200,50 0,1000,200,900 1920,0,1400,300 1920,1200,1400,900'  \
+            -blur 10x2 \
+            back-$image
 done
